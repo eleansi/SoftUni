283250d60f4230d8387f55cc5980cf32967dab08
Eleansi <markuckataa@gmail.com>
2015-11-29 16:41:03 +0200

    Exception handling Homework

---
+++ OOP - Work/OOP - Homework/Exception handling/EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterNumbers
+{
+    class EnterNumbers
+    {
+        static void Main(string[] args)
+        {
+            int start = Int32.Parse(Console.ReadLine());
+            int end = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i < 100; i++)
+            {
+                start = ReadNumber(start, end);
+            }
+        }
+
+        private static int ReadNumber(int start, int end)
+        {
+            int number;
+            try
+            {
+                Console.WriteLine("Enter number < {0} your number < {1}: ", start, end);
+                number = Int32.Parse(Console.ReadLine());
+                while (!(start < number && number < end))
+                {
+                    Console.WriteLine("Your number is not in range {0} - {1}", start, end);
+                    Console.WriteLine("Enter number < {0} your number < {1}: ", start, end);
+                    number = Int32.Parse(Console.ReadLine());
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number!");
+                throw;
+            }
+
+            return number;
+        }
+    }
+}
---
+++ OOP - Work/OOP - Homework/Exception handling/ExceptionHandl/SquareRoots.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandl
+{
+    class SquareRoots
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a number:");
+                int number = int.Parse(Console.ReadLine());
+                if (number < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                double result = Math.Sqrt(number);
+                Console.WriteLine("Square root from {0} is {1}", number, result);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            catch (ArgumentOutOfRangeException) 
+            {
+                Console.WriteLine("Number should be positive!");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye!");
+            }
+        }
+    }
+}
